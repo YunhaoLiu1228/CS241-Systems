146ca06dc392125b5e905b3fb4e3778631c63ac9
Emily Roller <eroller2@illinois.edu>
2020-09-14 21:56:03 -0500

    commented out test files

--- vector/sstring_test.c
+++ vector/sstring_test.c
@@ -6,53 +6,53 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    char* input = "this is a char pointer";
-    sstring* str = cstr_to_sstring(input);
+    // char* input = "this is a char pointer";
+    // sstring* str = cstr_to_sstring(input);
     
-    // printf("sstring: ");
-    // for (size_t i = 0; i < str->length; i++) {
-    //     printf("%c", **(char**)vector_at(str->string, i));
+    // // printf("sstring: ");
+    // // for (size_t i = 0; i < str->length; i++) {
+    // //     printf("%c", **(char**)vector_at(str->string, i));
+    // // }
+    // // printf("\n");
+
+    // char* output = sstring_to_cstr(str);
+    // printf("c_string: '%s' \n", output);
+    // printf(" // ---------------------------------------- // \n");
+
+    // // -------------- test append ---------------- //
+    // char* input2 = " hiyaaa";
+    // sstring* str_to_append = cstr_to_sstring(input2);
+    // sstring_append(str, str_to_append);
+
+    // char *cstr = sstring_to_cstr(str);
+    //  for (size_t i = 0; i < strlen(cstr); i++) {
+    //     printf("%c", cstr[i]);
     // }
     // printf("\n");
+    // printf(" // ---------------------------------------- // \n");
 
-    char* output = sstring_to_cstr(str);
-    printf("c_string: '%s' \n", output);
-    printf(" // ---------------------------------------- // \n");
+    // // --------------- test split ---------------- //
 
-    // -------------- test append ---------------- //
-    char* input2 = " hiyaaa";
-    sstring* str_to_append = cstr_to_sstring(input2);
-    sstring_append(str, str_to_append);
+    // char* strljs = "here is another";
+    // printf("Splitting string: %s\n", strljs);
+    // sstring* str_ = cstr_to_sstring(strljs);
+    // vector* subs = sstring_split(str_, ' ');
 
-    char *cstr = sstring_to_cstr(str);
-     for (size_t i = 0; i < strlen(cstr); i++) {
-        printf("%c", cstr[i]);
-    }
-    printf("\n");
-    printf(" // ---------------------------------------- // \n");
+    // for(size_t i = 0; i < vector_size(subs); i++) {
+    //     printf("substr: %s\n", *(char**)vector_at(subs, i));
+    // }
 
-    // --------------- test split ---------------- //
+    // char* strlja = "some/strings/to/split";
+    // printf("Splitting string: %s\n", strlja);
+    // sstring* strs_ = cstr_to_sstring(strlja);
+    // vector* subss = sstring_split(strs_, '/');
 
-    char* strljs = "here is another";
-    printf("Splitting string: %s\n", strljs);
-    sstring* str_ = cstr_to_sstring(strljs);
-    vector* subs = sstring_split(str_, ' ');
+    // for(size_t i = 0; i < vector_size(subss); i++) {
+    //     printf("substr: %s\n", *(char**)vector_at(subss, i));
+    // }
+    // printf(" // ---------------------------------------- // \n");
 
-    for(size_t i = 0; i < vector_size(subs); i++) {
-        printf("substr: %s\n", *(char**)vector_at(subs, i));
-    }
-
-    char* strlja = "some/strings/to/split";
-    printf("Splitting string: %s\n", strlja);
-    sstring* strs_ = cstr_to_sstring(strlja);
-    vector* subss = sstring_split(strs_, '/');
-
-    for(size_t i = 0; i < vector_size(subss); i++) {
-        printf("substr: %s\n", *(char**)vector_at(subss, i));
-    }
-    printf(" // ---------------------------------------- // \n");
-
-    // ---------------- test slice ---------------- //
+    // // ---------------- test slice ---------------- //
 
     return 0;
 }
