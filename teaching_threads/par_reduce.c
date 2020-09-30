8f0f7fc5c6e0974fca833e5fe08ce9cfaad5d063
Emily Roller <eroller2@illinois.edu>
2020-09-30 18:34:52 -0500

    threads

--- teaching_threads/par_reduce.c
+++ teaching_threads/par_reduce.c
@@ -13,53 +13,44 @@
 
 /* You might need a struct for each task ... */
 typedef struct task_t {
-    int* data;
-    size_t num_items;
+    int* list_;
+    size_t list_len_;
+    reducer reduce_func_;
+    int base_case_;
 } task_t;
+
+
+void* my_func(void* argv) {
+    int* retval = malloc(sizeof(int));
+    task_t* t = argv;
+    
+    *retval = reduce(t->list_, t->list_len_, t->reduce_func_, t->base_case_);
+
+    pthread_exit( retval);
+
+    return NULL;
+}
+
  
-/* You should create a start routine for your threads. */
-/**
- * multi-threaded solution to reduce().
- *
- * This method takes in a `list` of ints and returns a int that is a "reduced"
- * version of the list, but does so with `num_threads` threads.
- *
- * Note: that this function DOES NOT modify the original list.
- *
- * `list`- is a pointer to the begining of an array of ints.
- * `length` - is how many ints are in the array of ints.
- * `reducer_func` - is the reducer used to transform a list of ints to an int.
- * `base_case` - is the base case that the reducer will use.
- * `num_threads` - is how many threads (in addition to the main thread) are used
- * in the parallelization.
- */
+
 int par_reduce(int* list, size_t list_len, reducer reduce_func, int base_case,
                size_t num_threads) {
 
-    int result = base_case;
+    pthread_t id;
+    int* r;
 
-    // the number of elements of the array that each p_thread will handle
-    size_t frac = list_len/num_threads;
+    task_t* task = malloc(sizeof(task_t));
+    task->list_ = list;
+    task->list_len_ = list_len;
+    task->reduce_func_ = reduce_func;
+    task->base_case_ = base_case;
 
-    // make the p_threads
-    pthread_t tids[num_threads]; 
 
-    for (size_t i = 0; i < num_threads; i++) {
-        task_t* t = malloc(sizeof(task_t));
-        if (t == NULL) {
-            printf("Uh oh\n");
-            exit(1);
-        }
-        t->data = list;
-        t->num_items = frac;
-
-        pthread_create(&tids[i], NULL, (void*)reduce_func, (void *)t);
+    for (size_t i = 0; i < num_threads; ++i) {
+        pthread_create(&id, NULL, my_func, task);
     }
 
-    // join the threads
-    for (size_t i = 0; i < num_threads; i++) {
-        pthread_join(tids[i], NULL);
-    }
-    printf("result: %d\n", result);
-    return result;
+    pthread_join(id, (void**)&r);
+
+    return *r;
 }
