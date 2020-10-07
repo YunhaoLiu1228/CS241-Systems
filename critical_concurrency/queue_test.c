adf57c72535ad9b88f536d0a347cb8fcdfc3fec9
Emily Roller <eroller2@illinois.edu>
2020-10-07 13:23:23 -0500

    semamore

--- critical_concurrency/queue_test.c
+++ critical_concurrency/queue_test.c
@@ -10,25 +10,45 @@
 
 #include "queue.h"
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("usage: %s test_number return_code\n", argv[0]);
-        exit(1);
-    }
-    queue* q = queue_create(10);
+static queue* q;
 
-    int in[10] = {1, 2, -3, -4, 5, 60, -70, 8, -9, 1000};
-
+void* generator(void* arg) {
+    int in[10] = {1,2,3,4,5,6,7,8,9,0};
     for (int i = 0; i < 10; i++) {
-        printf("pushing: %d\n", in[i]);
         queue_push(q, &in[i]);
+        printf("pushing: %d\n", in[i]);
     }
-    
-    puts("- - - - - - - - - - - - - - - -");
+    return NULL;
+}
 
-    for (int i = 0; i < 10; i++) {
+void* consumer(void* arg) {
+    for (int i = 0;i < 10; i++) {
         printf("pulling: %d\n", *(int*)queue_pull(q));
     }
+    return NULL;
+}
+
+int main(int argc, char **argv) {
+    // if (argc != 3) {
+    //     printf("usage: %s test_number return_code\n", argv[0]);
+    //     exit(1);
+    // }
+    q = queue_create(10);
+
+    pthread_t pt;
+
+    for (int i = 0; i < 3; i++) {
+        pthread_create(&pt, NULL, generator, NULL);
+        
+        
+    }
+    
+
+    pthread_t pt_2;
+    pthread_create(&pt_2, NULL, consumer, NULL);
+
+    pthread_join(pt, NULL);
+    pthread_join(pt_2, NULL);
 
     queue_destroy(q);
     return 0;
--- critical_concurrency/semamore.c
+++ critical_concurrency/semamore.c
@@ -29,7 +29,16 @@ void semm_init(Semamore *s, int value, int max_val) {
  *  Otherwise, should decrement the value.
  */
 void semm_wait(Semamore *s) {
-    /* Your code here */
+    pthread_mutex_lock(&(s->m));
+
+    while (s->value <= 0) {
+        pthread_cond_wait(&(s->cv), &(s->m));
+    }
+
+    s->value--;
+
+    pthread_cond_broadcast(&(s->cv));
+    pthread_mutex_unlock(&(s->m));
 }
 
 /**
@@ -38,7 +47,16 @@ void semm_wait(Semamore *s) {
  *  Otherwise, should increment the value.
  */
 void semm_post(Semamore *s) {
-    /* Your code here */
+    pthread_mutex_lock(&(s->m));
+
+    while (s->value == s->max_val) {
+        pthread_cond_wait(&(s->cv), &(s->m));
+    }
+
+    s->value++;
+    
+    pthread_cond_broadcast(&(s->cv));
+    pthread_mutex_unlock(&(s->m));
 }
 
 /**
@@ -47,5 +65,7 @@ void semm_post(Semamore *s) {
  * The actual Semamore struct must be freed by the user.
  */
 void semm_destroy(Semamore *s) {
-    /* Your code here */
+    pthread_mutex_destroy(&(s->m));
+    pthread_cond_destroy(&(s->cv));
+    //DON'T free s!
 }
