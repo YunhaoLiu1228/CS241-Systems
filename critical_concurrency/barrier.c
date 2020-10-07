ff8e61164ce46bec61c06eaf4f2842346dfd4eb6
Emily Roller <eroller2@illinois.edu>
2020-10-07 18:13:32 -0500

    barrier tests 1 and 2

--- critical_concurrency/barrier.c
+++ critical_concurrency/barrier.c
@@ -9,8 +9,9 @@
 int barrier_destroy(barrier_t *barrier) {
     if (!barrier) return 1;
 
+    pthread_mutex_destroy(&(barrier->mtx)); 
     pthread_cond_destroy(&(barrier->cv));
-    pthread_mutex_destroy(&(barrier->mtx));
+    
     return 0;
 }
 
@@ -29,17 +30,31 @@ int barrier_init(barrier_t *barrier, unsigned int num_threads) {
 }
 
 int barrier_wait(barrier_t *barrier) {
+    // [pop it] lock it polka dot it
     pthread_mutex_lock(&(barrier->mtx));
+
+    while (barrier->times_used == 0) {
+        pthread_cond_wait(&(barrier->cv), &(barrier->mtx));
+    }
 
     barrier->count++;
     
     if (barrier->count == barrier->n_threads) {
-        pthread_cond_broadcast(&(barrier->cv));
+        barrier->times_used = 0;
+        barrier->count--;
+        
+    } else {
+        while (barrier->times_used == 1 && barrier->count != barrier->n_threads) {
+            pthread_cond_wait(&(barrier->cv), &(barrier->mtx));
+        }
+
+        barrier->count--;
+
+        if (barrier->count == 0) barrier->times_used = 1;
+
     }
 
-    while (barrier->count < barrier->n_threads) {
-        pthread_cond_wait(&(barrier->cv), &(barrier->mtx));
-    }
+    pthread_cond_broadcast(&(barrier->cv));
     pthread_mutex_unlock(&barrier->mtx);
     return 0;
 }
