89b51605474d679c5d770577c3b65eb3acfdca59
Emily Roller <eroller2@illinois.edu>
2020-10-22 23:54:55 -0500

    echo A

--- parallel_make/parmake.c
+++ parallel_make/parmake.c
@@ -13,16 +13,28 @@
 #include <stdio.h>
 
 int parmake(char *makefile, size_t num_threads, char **targets) {
-    graph* graph = parser_parse_makefile(makefile, targets);
+    graph* dependency_graph = parser_parse_makefile(makefile, targets);
     queue* bfs = queue_create(0);
 
-    vector* vertices = graph_vertices(graph);
-    queue_push(bfs, vector_front(vertices));
+    // list of all targets
+    vector* graph_targets = graph_vertices(dependency_graph);
 
+    rule_t* rule = (rule_t*) graph_get_vertex_value(dependency_graph, *vector_back(graph_targets));
+    //printf("rule target: %s\n", rule->target);
 
-    vector_destroy(vertices);
+    char* command = malloc(sizeof(char));
+    command = *(char**)vector_front(rule->commands);
+    //printf("rule command: %s\n", *(char**)vector_front(rule->commands));
+
+    int status;
+    if (( status = system(command) != 0 )) {
+        return status;
+    };
+
+    free(command);
+    vector_destroy(graph_targets);
     queue_destroy(bfs);
-    graph_destroy(graph);
+    graph_destroy(dependency_graph);
 
     return 0;
 }
