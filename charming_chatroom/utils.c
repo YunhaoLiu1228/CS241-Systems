8ad589e88d6d24b1c991309924133e9608ebe6c9
Emily Roller <eroller2@illinois.edu>
2020-11-03 19:52:10 -0600

    lab chatroom?

--- charming_chatroom/utils.c
+++ charming_chatroom/utils.c
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
 
 #include "utils.h"
 static const size_t MESSAGE_SIZE_DIGITS = 4;
@@ -31,16 +33,74 @@ ssize_t get_message_size(int socket) {
 
 // You may assume size won't be larger than a 4 byte integer
 ssize_t write_message_size(size_t size, int socket) {
-    // Your code here
-    return 9001;
+    int32_t message_size = htonl(size);
+
+    ssize_t write_bytes = write_all_to_socket(socket, (char*) &message_size, MESSAGE_SIZE_DIGITS);
+
+    return write_bytes;
 }
 
+/**
+ 
+while number of bytes is not the number needed:
+    return_code = read bytes from socket
+    if return_code = 0:
+        return bytes read
+    else if return_code > 0:
+        add return_code bytes to counter
+    else if return_code == -1 and error was interrupted:
+        try again
+    else:
+        return -1
+return bytes read
+
+**/
 ssize_t read_all_from_socket(int socket, char *buffer, size_t count) {
-    // Your Code Here
-    return 9001;
+    ssize_t read_bytes = 0;
+    ssize_t remaining_bytes = count;
+    ssize_t return_code;
+
+    while (read_bytes != 0 ) {
+        return_code = read(socket, (void*) (buffer + read_bytes), remaining_bytes);
+
+        if (return_code == 0) {
+            return read_bytes;
+
+        } else if (return_code > 0) {
+            read_bytes += return_code;
+            remaining_bytes -= return_code;
+        } else if (return_code == -1 && errno == EINTR) {
+            continue;
+        } else {
+            return -1;
+        }
+    }
+    return read_bytes;
 }
 
 ssize_t write_all_to_socket(int socket, const char *buffer, size_t count) {
-    // Your Code Here
-    return 9001;
+
+    ssize_t read_bytes = 0;
+    ssize_t remaining_bytes = count;
+    ssize_t return_code;
+
+    while (read_bytes != 0 ) {
+        return_code = write(socket, (void*) (buffer + read_bytes), remaining_bytes);
+
+        if (return_code == 0) {
+            return read_bytes;
+
+        } else if (return_code > 0) {
+            read_bytes += return_code;
+            remaining_bytes -= return_code;
+
+        } else if (return_code == -1 && errno == EINTR) {
+            continue;
+
+        } else {
+            return -1;
+            
+        }
+    }
+    return read_bytes;
 }
