eda9c58c57f08d47f63ee8d295f6e08093875664
Emily Roller <eroller2@illinois.edu>
2020-11-13 20:53:45 -0600

    start of client.c

--- nonstop_networking/client.c
+++ nonstop_networking/client.c
@@ -3,20 +3,91 @@
  * CS 241 - Fall 2020
  */
 #include "format.h"
+
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #include "common.h"
 
 char **parse_args(int argc, char **argv);
 verb check_args(char **args);
+
+// my functions
+int connect_to_server(char* host, char* port);
+int execute_request(verb request);
+
+
 int main(int argc, char **argv) {
-    // Good luck!
+
+    // parse my args into string
+    char** my_args = parse_args(argc, argv);
+
+    // convert string to verb enum
+    verb request = check_args(my_args);
+
+    // make connection to server
+    if (connect_to_server(my_args[0], my_args[1]) == 1) {
+        exit(1);
+    }
+
+    // execute the request
+    if (execute_request(request) == 1) {
+        exit(1);
+    }
+
+    // yay!
+    return 0;
+
 }
+
+int connect_to_server(char* host, char* port) {
+
+    struct addrinfo hints, *result;
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int get_addr_info = getaddrinfo(host, port, &hints, &result);
+
+    if (get_addr_info) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(get_addr_info));
+        return 1;
+    }
+
+    int sock_fd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (sock_fd == -1) {
+        perror("socket()");
+        return 1;
+    }
+
+    int connection = connect(sock_fd, result->ai_addr, result->ai_addrlen);
+    if (connection == -1) {
+        perror("connect()");
+        return 1;
+    } 
+
+    freeaddrinfo(result);
+
+    return 0;
+}
+
+
+int execute_request(verb request) {
+    return 0;
+}
+
+
 
 /**
  * Given commandline argc and argv, parses argv.
