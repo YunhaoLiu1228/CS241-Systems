060beec026efba345d48a59a35e1f084890b35c2
Emily Roller <eroller2@illinois.edu>
2020-11-17 18:46:38 -0600

    lookup1

--- mad_mad_access_patterns/lookup1.c
+++ mad_mad_access_patterns/lookup1.c
@@ -4,6 +4,10 @@
  */
 #include "tree.h"
 #include "utils.h"
+#include "string.h"
+
+#include <stdlib.h>
+
 /*
   Look up a few nodes in the tree and print the info they contain.
   This version uses fseek() and fread() to access the data.
@@ -11,6 +15,80 @@
   ./lookup1 <data_file> <word> [<word> ...]
 */
 
-int main(int argc, char **argv) {
+
+int validate_btre(FILE* input) {
+
+  char buff[4];
+  fread(buff, 1, 4, input);
+
+  if (strcmp(buff, "BTRE") == 0) {
+    return 1;
+  }
+
+  return 0;
+}
+
+int word_search(FILE* inputfile, char* word, uint32_t offset) {
+
+  if (offset == 0) {  // base case
+    printNotFound(word);
     return 0;
+  }
+
+  fseek(inputfile, offset, SEEK_SET);
+  
+  BinaryTreeNode btnode;
+  fread(&btnode, sizeof(BinaryTreeNode), 1, inputfile);
+  fseek(inputfile, sizeof(BinaryTreeNode) + offset, SEEK_SET);
+
+  char wordInFile[30];
+  fread(wordInFile, 30, 1, inputfile);
+
+  if (strcmp(word, wordInFile) == 0) {
+    printFound(wordInFile, btnode.count, btnode.price);
+    return 1;
+  }
+    
+  else if (strcmp(word, wordInFile) > 0) {
+    if (word_search(inputfile,  word, btnode.right_child)) {
+      return 1;
+    }
+  }
+
+  else if (strcmp(word, wordInFile) < 0) {
+    if (word_search(inputfile,  word, btnode.left_child)) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+int main(int argc, char **argv) {
+
+  if (argc < 2) {
+    printArgumentUsage();
+    exit(1);
+  }
+
+  char* in = strdup(argv[1]);
+  FILE* inputfile = fopen(in, "r");
+
+  if (!inputfile) {
+    openFail(in);
+    exit(2);
+  }
+
+  if (!validate_btre(inputfile)) {
+    formatFail(in);
+    exit(2);
+  }
+
+  for (int i = 2; i < argc; i++) {
+    word_search(inputfile, argv[i], BINTREE_ROOT_NODE_OFFSET);
+  }
+
+  fclose(inputfile);
+  free(in);
+  return 0;
 }
