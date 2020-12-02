1186fc5405329e25e3e11c901ea9c7333d622ade
Emily Roller <eroller2@illinois.edu>
2020-12-02 14:16:20 -0600

    comparers

--- nonstop_networking/client.c
+++ nonstop_networking/client.c
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
+#include <errno.h
 #include <netdb.h>
 #include <pthread.h>
 #include <signal.h>
--- savvy_scheduler/libscheduler.c
+++ savvy_scheduler/libscheduler.c
@@ -18,9 +18,12 @@
  */
 typedef struct _job_info {
     int id;
-
-    /* TODO: Add any other information and bookkeeping you need into this
-     * struct. */
+    int arrival_t;
+    int priority;
+    int running_t;
+    int remaining_t;
+    int roundr_t;
+    
 } job_info;
 
 void scheduler_start_up(scheme_t s) {
@@ -52,13 +55,24 @@ void scheduler_start_up(scheme_t s) {
     // Put any additional set up code you may need here
 }
 
+/*
+ * The following comparers can return -1, 0, or 1 based on the following:
+ *  -1 if 'a' comes before 'b' in the ordering.
+ *  1 if 'b' comes before 'a' in the ordering.
+ *  0 if break_tie() returns 0.
+ */
+
 static int break_tie(const void *a, const void *b) {
     return comparer_fcfs(a, b);
 }
 
 int comparer_fcfs(const void *a, const void *b) {
-    // TODO: Implement me!
-    return 0;
+    job_info* ja = (((job*) a)->metadata);
+    job_info* jb = (((job*) b)->metadata);
+
+    if (ja->arrival_t == jb->arrival_t) return 0;
+    else if (ja->arrival_t < jb->arrival_t) return -1;
+    else return 1;
 }
 
 int comparer_ppri(const void *a, const void *b) {
@@ -66,31 +80,57 @@ int comparer_ppri(const void *a, const void *b) {
     return comparer_pri(a, b);
 }
 
+// lower priority number runs first
 int comparer_pri(const void *a, const void *b) {
-    // TODO: Implement me!
-    return 0;
+    job_info* ja = (((job*) a)->metadata);
+    job_info* jb = (((job*) b)->metadata);
+
+    if (ja->priority == jb->priority) return break_tie(a, b);
+    else if (ja->priority < jb->priority) return -1;
+    else return 1;
+    
 }
 
 int comparer_psrtf(const void *a, const void *b) {
-    // TODO: Implement me!
-    return 0;
+    job_info* ja = (((job*) a)->metadata);
+    job_info* jb = (((job*) b)->metadata);
+
+    if (ja->remaining_t == jb->remaining_t) return break_tie(a, b);
+    else if (ja->remaining_t < jb->remaining_t) return -1;
+    else return 1;
 }
 
 int comparer_rr(const void *a, const void *b) {
-    // TODO: Implement me!
-    return 0;
+    job_info* ja = (((job*) a)->metadata);
+    job_info* jb = (((job*) b)->metadata);
+
+    if (ja->roundr_t == jb->roundr_t) return break_tie(a, b);
+    else if (ja->roundr_t < jb->roundr_t) return -1;
+    else return 1;
 }
 
 int comparer_sjf(const void *a, const void *b) {
-    // TODO: Implement me!
-    return 0;
+    job_info* ja = (((job*) a)->metadata);
+    job_info* jb = (((job*) b)->metadata);
+
+    if (ja->running_t == jb->running_t) return break_tie(a, b);
+    else if (ja->running_t < jb->running_t) return -1;
+    else return 1;
 }
 
 // Do not allocate stack space or initialize ctx. These will be overwritten by
 // gtgo
 void scheduler_new_job(job *newjob, int job_number, double time,
                        scheduler_info *sched_data) {
-    // TODO: Implement me!
+    
+    job_info* info = malloc(sizeof(job_info));
+    info->id = job_number;
+    info->arrival_t = time;
+    info->priority = sched_data->priority;
+    info->running_t = sched_data->running_time;
+
+    newjob->metadata = info;
+    
 }
 
 job *scheduler_quantum_expired(job *job_evicted, double time) {
